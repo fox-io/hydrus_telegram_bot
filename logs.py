0967d814e0bd8256945018ff5413dad734e31fc7
John M. <github.fox-io@foxdata.io>
2025-03-10 21:38:30 -0500

    Remove duplicate setLevel call.

--- logs.py
+++ logs.py
@@ -14,9 +14,6 @@ class Logs:
         # Set up rotating log files
         file_handler = RotatingFileHandler(out_file, maxBytes=5*1024*1024, backupCount=3)
 
-        # Set the logging level
-        logger.setLevel(logging.DEBUG)
-
         # Set the message formatting
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         formatter.datefmt = '%Y-%m-%d %H:%M:%S'
