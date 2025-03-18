265db8d7ee34c2703a729253f12eef4f77dcb3a3
John M. <github.fox-io@foxdata.io>
2025-03-18 14:17:52 -0500

    Documentation

--- modules/file_manager.py
+++ modules/file_manager.py
@@ -2,11 +2,31 @@ from modules.log_manager import LogManager
 import json
 
 class FileManager:
+    """
+    FileManager handles reading and writing json data to files.
+
+    Methods:
+        operation(filename, mode, payload): Reads or writes json data to a file.
+    """
     def __init__(self):
+        """
+        Initializes the FileManager object.
+        """
         self.logger = LogManager.setup_logger('FIL')
         self.logger.debug('File Module initialized.')
 
     def operation(self, filename: str, mode: str, payload=None):
+        """
+        Reads or writes json data to a file.
+
+        Args:
+            filename (str): The name of the file to read or write.
+            mode (str): The mode to open the file in.
+            payload (dict, optional): The data to write to the file.
+
+        Returns:
+            dict: The json data read from the file.
+        """
         try:
             with open(filename, mode) as file:
                 if 'r' in mode:
