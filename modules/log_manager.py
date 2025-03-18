3c5939f1afc6b74580f129052c9a3ce694f7742c
John M. <github.fox-io@foxdata.io>
2025-03-18 10:30:27 -0500

    Allow log_manager to create log folder if it does not exist.

--- modules/log_manager.py
+++ modules/log_manager.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 from colorama import init, Fore, Style
+import os
 
 # Initialize colorama
 init(autoreset=True)
@@ -25,29 +26,32 @@ class LogManager:
                 log_color = self.COLORS.get(record.levelname, Fore.WHITE)
                 log_message = super().format(record)
                 return f"{log_color}{log_message}{Style.RESET_ALL}"
-
+            
         # Prevent duplicate loggers from beging created
         if logger.hasHandlers():
             return logger
-
+        
+        # Create the log directory if it doesn't exist
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        
         # Set up rotating log files
         file_handler = RotatingFileHandler(out_file, maxBytes=5*1024*1024, backupCount=3)
-
+        
         # Set the message formatting
         formatter = logging.Formatter('%(asctime)s (%(name)s) %(levelname)s - %(message)s')
         formatter.datefmt = '%Y-%m-%d %H:%M:%S'
-
+        
         # Create the handlers
         console_handler = logging.StreamHandler()
-
+        
         # Set the handler levels
         file_handler.setLevel(logging.DEBUG)
         console_handler.setLevel(logging.DEBUG)
-
+        
         # Set the handler formatting
         file_handler.setFormatter(formatter)
         console_handler.setFormatter(ColorFormatter('%(asctime)s (%(name)s) %(levelname)s - %(message)s'))
-
+        
         # Add the handlers to the logger
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
