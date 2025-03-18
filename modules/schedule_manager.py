724f18a3b399cc20e920f5e79eb2772cbae657d5
John M. <github.fox-io@foxdata.io>
2025-03-18 13:51:55 -0500

    Documentation

--- modules/schedule_manager.py
+++ modules/schedule_manager.py
@@ -3,7 +3,23 @@ import time
 from modules.log_manager import LogManager
 
 class ScheduleManager:
+    """
+    ScheduleManager handles the scheduling of updates at specified intervals.
+
+    Attributes:
+        timezone (int): The timezone offset in hours. Used to display the next update time.
+        delay (int): The delay between updates in minutes.
+        scheduler (sched.scheduler): The scheduler object.
+        logger (Logger): The logger
+    """
     def __init__(self, timezone, delay):
+        """
+        Initializes the ScheduleManager object.
+
+        Args:
+            timezone (int): The timezone offset in hours. Used to display the next update time.
+            delay (int): The delay between updates in minutes.
+        """
         self.logger = LogManager.setup_logger('SCH')
         self.timezone = timezone
         self.delay = delay
@@ -11,18 +27,30 @@ class ScheduleManager:
         self.logger.debug('Scheduler Module initialized.')
 
     def get_next_update_time(self):
-        # Calculate the next update time.
+        """
+        Returns the next update time based on the current time, timezone, and delay.
+
+        Returns:
+            float: The next update time
+        """
         current_time = (time.time() + ((60 * 60) * self.timezone))
         return (current_time - (current_time % (self.delay * 60))) + (self.delay * 60)
 
     def schedule_update(self, callback):
-        # Schedules an event for the next update time.
+        """
+        Schedules the next update.
+
+        Args:
+            callback (function): The function to call when the update is scheduled
+        """
         next_time = self.get_next_update_time() - (3600 * self.timezone)
         self.logger.info(f"Next update scheduled for {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(next_time))}.")
         self.scheduler.enterabs(next_time, 1, callback, ())
 
     def run(self):
-        # Run the scheduler.
+        """
+        Runs the scheduler indefinitely.
+        """
         while True:
             try:
                 self.scheduler.run(blocking=False)
